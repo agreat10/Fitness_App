a5edfb12f1e3a59c28ddcc46cc4f67254ed54b30
agreat10 <komolov10@gmail.com>
2023-11-08 13:05:25 +0300

    Добавлено модульное тестирование  для контроллера пользователя

--- Fitness_App.BL/Controller/UserController.cs
+++ Fitness_App.BL/Controller/UserController.cs
@@ -62,7 +62,7 @@ namespace Fitness_App.BL.Controller
             var formatter = new BinaryFormatter();
             using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
             {
-                if (formatter.Deserialize(fs) is List<User> users)
+                if (fs.Length >0 && formatter.Deserialize(fs) is List<User> users)
                 {
                     return users;
                 }
---
+++ Fitness_App.BLTests/Controller/UserControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fitness_App.BL.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness_App.BL.Controller.Tests
+{
+    [TestClass()]
+    public class UserControllerTests
+    {
+        
+
+        [TestMethod()]
+        public void SetNewUserDataTest()
+        {
+            //Arrange
+            var userName = Guid.NewGuid().ToString();
+            var gender = "man";
+            var birthDate = DateTime.Now.AddYears(-18);
+            double weight = 90;
+            double height = 190;
+            //Act
+            var controller = new UserController(userName);
+            controller.SetNewUserData(gender, birthDate, weight, height);
+            var controller2 = new UserController(userName);
+            //Assert
+            Assert.AreEqual(userName, controller2.CurrentUser.Name);
+        }
+
+        [TestMethod()]
+        public void SaveTest()
+        {
+            //Arrange
+            var userName = Guid.NewGuid().ToString();
+
+            //Act
+            var controller = new UserController(userName);
+
+            //Assert
+            Assert.AreEqual(userName, controller.CurrentUser.Name);
+
+        }
+    }
+}
